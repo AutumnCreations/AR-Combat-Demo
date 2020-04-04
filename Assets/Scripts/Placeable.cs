f8eeef5b68b5c0a80cfbe964a294f05c7f159459
Addikins <addikinsplays@gmail.com>
2020-04-04 16:10:44 -0700

    Initial Commit

---
+++ Assets/Scripts/Placeable.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] GameObject placementIndicator = null;
+    [SerializeField] GameObject objectToPlace = null;
+
+    ARSessionOrigin origin;
+    Pose placementPose;
+
+    bool isPlacementValid;
+
+    void Start()
+    {
+        origin = FindObjectOfType<ARSessionOrigin>();
+    }
+
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+        if (isPlacementValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            PlaceObject();
+        }
+    }
+
+    private void PlaceObject()
+    {
+        Instantiate(objectToPlace, placementPose.position, placementPose.rotation);
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if (isPlacementValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    private void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+
+        origin.GetComponent<ARRaycastManager>().Raycast(screenCenter, hits, TrackableType.Planes);
+
+        isPlacementValid = hits.Count > 0;
+        if (isPlacementValid)
+        {
+            placementPose = hits[0].pose;
+
+            var cameraForward = Camera.current.transform.forward;
+            var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+            placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+        }
+    }
+}
+

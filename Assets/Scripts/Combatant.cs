fd58f6fa85d100e6a1f1057880e3f84fd921bcf5
Addikins <addikinsplays@gmail.com>
2020-04-05 13:25:29 -0700

    Add speed slider for combatants

---
+++ Assets/Scripts/Combatant.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combatant : MonoBehaviour
+{
+    float speed = 0f;
+
+    Animator animator;
+
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        print(speed);
+    }
+
+    public void SetSpeed(float fSpeed)
+    {
+        speed = fSpeed;
+        animator.SetFloat("forwardSpeed", speed);
+    }
+}
---
+++ Assets/Scripts/SpeedSlider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedSlider : MonoBehaviour
+{
+    Combatant[] combatants;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        combatants = FindObjectsOfType<Combatant>();
+    }
+
+    public void AdjustSpeed(float speedToSet)
+    {
+        foreach (Combatant combatant in combatants)
+        {
+            combatant.SetSpeed(speedToSet);
+        }
+    }
+}
